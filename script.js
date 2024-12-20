f0f2df0c6845cd511c0c048d7829319fee1c00dd
Artem <agaleev583@gmail.com>
2024-12-20 17:52:26 +0300

    feat: add condicion

--- script.js
+++ script.js
@@ -11,4 +11,15 @@ function submitRequest(event) {
     const value = input.value;
     const searchEngine = select.value;
 
+    let params;
+
+    if (searchEngine === "google" || searchEngine === "бинг") {
+        params = "/search?q=";
+    } else if (searchEngine === "яндекс") {
+        params = "/search?text=";
+    }else if (searchEngine === "что это такое!?") {
+        params = "?q=";
+    }
+
+
 }
